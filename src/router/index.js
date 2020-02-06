d4ca6d714f1289e5d409a54b16ee234a448b9321
GiovaniGil <giovani_paulino@hotmail.com>
2020-02-06 12:53:50 -0300

    Personalização

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import PedidoView from '../views/PedidoView.vue'
+import PersonalizarView from '../views/PersonalizarView.vue'
 
 Vue.use(VueRouter)
 
@@ -9,10 +10,24 @@ const routes = [
     path: '/',
     name: 'pedido',
     component: PedidoView
+  },
+  {
+    path: '/personalizar',
+    name: 'personalizar',
+    component: PersonalizarView
+  },
+  {
+    method: 'GET',
+    path: 'personalizacoes/',
+    response: [
+      { id: 14 },
+      { id: 22 }
+    ]
   }
 ]
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 })
 
